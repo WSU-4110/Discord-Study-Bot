f47fa27034db95e7cb1198723d180cdbd39d76db
Syed Safwaan <syed-safwaan@wayne.edu>
2021-02-15 14:30:35 -0500

    Edited Timer class [FR. 2] (lead: syed-safwaan)

--- utils/timer.py
+++ utils/timer.py
@@ -1,22 +1,42 @@
 import datetime as dt
-import time
 from datetime import timedelta as td
 
 
 class Timer:
     def __init__(self, userid: str, initial_time: int, msg: str):
-        self.userid = userid
-        self.start_time = dt.datetime.now()
-        self.end_time = self.start_time + td(minutes=initial_time)
-        self.msg = msg
-
-    def time_remaining(self):
-        time.sleep(5)
-        return self.end_time - dt.datetime.now()
+        self._userid = userid
+        self._start_time = dt.datetime.now()
+        self._end_time = self.start_time + td(minutes=initial_time)
+        self._msg = msg
 
     def __repr__(self):
-        return f"Timer (User: {self.userid}) [ {self.end_time} | \"{self.msg}\" | Created at {self.start_time} | With " \
-               f"{self.time_remaining()} time remaining]"
+        return f"Timer (User: {self.userid}) " \
+               f"[ {self.end_time} | \"{self.msg}\" " \
+               f"| Created at {self.start_time} " \
+               f"| With {self.time_remaining()} time remaining]"
+
+    @property
+    def userid(self):
+        return self._userid
+
+    @property
+    def start_time(self):
+        return self._start_time
+
+    @property
+    def end_time(self):
+        return self._end_time
+
+    @property
+    def msg(self):
+        return self._msg
+
+    @msg.setter
+    def msg(self, new_msg):
+        self._msg = new_msg
+
+    def time_remaining(self):
+        return self.end_time - dt.datetime.now()
 
     def embed_repr(self):
         pass
