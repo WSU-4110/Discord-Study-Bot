3f4f130af246d07e7614504c0722ff6f3ca8406e
Adi Sai <adithya.sairam1@gmail.com>
2021-02-17 20:20:21 -0500

    Allow for custom messages to be sent to the user

--- utils/async_tasks.py
+++ utils/async_tasks.py
@@ -3,15 +3,16 @@ import datetime as dt
 from utils import config
 
 
-async def handle_timers():
+async def handle_timers(message_to_send=None):
     while True:
         top_timer = config.timer_pqueue.peek()
         print(top_timer.time_remaining())
         if dt.datetime.now() >= top_timer.end_time:
             timers_to_fire = config.timer_pqueue.get_all_tasks_to_fire()
             for timer in timers_to_fire:
-                message_to_send = f"{timer.discord_message.author.mention} Your timer for {str(timer.end_time)} has finished. " \
-                                  f"'Here's your initial message: {timer.msg}"
+                if message_to_send is None:
+                    message_to_send = f"{timer.discord_message.author.mention} Your timer for {str(timer.end_time)} has finished. " \
+                                      f"'Here's your initial message: {timer.msg}"
                 await timer.discord_message.channel.send(message_to_send)
         await asyncio.sleep(10)
         if len(config.timer_pqueue) == 0:
