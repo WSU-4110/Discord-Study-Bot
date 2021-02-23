9dbdd70d70e7a0f47adbc50813e143422ffc5daf
Adi Sai <adithya.sairam1@gmail.com>
2021-02-23 17:30:00 -0500

    [FR2] update Timer and Timer Priority Queue to enable UTC times (lead: adisai)

--- utils/timer_priority_queue.py
+++ utils/timer_priority_queue.py
@@ -5,12 +5,14 @@ from collections import Counter
 
 
 class TimerPriorityQueue(PriorityQueue):
+    """A derived class of the in-built PriorityQueue used to accommodate Timer objects"""
     def __init__(self):
         super().__init__()
-        self.alarm_map = Counter()
-        self.user_map = {}
+        self.alarm_map = Counter()  # a Counter object used to keep track of all the timers to fire at a certain point in time
+        self.user_map = {}  # a map used to keep track of all the timers for a given user
 
     def add_task(self, a_time: timer.Timer):
+        """Function to add a timer-derived object to the queue"""
         self.alarm_map[a_time.end_time] += 1
         if a_time.userid not in self.user_map.keys():
             self.user_map[a_time.userid] = []
@@ -18,6 +20,7 @@ class TimerPriorityQueue(PriorityQueue):
         self.put(a_time)
 
     def get_all_tasks_to_fire(self):
+        """Function to get the all the timer-derived objects that will fire at the end time of the top timer"""
         tasks_arr = []
         num_items = self.alarm_map[self.peek().end_time]
         for i in range(num_items):
@@ -25,10 +28,12 @@ class TimerPriorityQueue(PriorityQueue):
         return tasks_arr
 
     def get_top_task(self):
+        """Function to get (retrieve and remove) the top timer-derived object from the queue"""
         top_item = self.get()
         return top_item
 
     def peek(self):
+        """Function to peek (retrieve) the top timer-derived object from the queue"""
         if self.empty():
             pass
         else:
@@ -37,12 +42,15 @@ class TimerPriorityQueue(PriorityQueue):
             return top_item
 
     def get_all_tasks(self):
+        """Function to get the Counter object to retrieve all the timer-derived objects"""
         return self.alarm_map
 
     def get_tasks_by_user(self, user_id):
+        """Function to get the Counter object to retrieve all the timer-derived objects for a given user"""
         return self.user_map[user_id]
 
     def __len__(self):
+        """Function to get the size of this queue"""
         return self.qsize()
 
 
