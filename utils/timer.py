01a6ebb5f84fd8e252fd8ac7ef7cd68e6a406dc1
Syed Safwaan <syed-safwaan@wayne.edu>
2021-02-24 16:07:56 -0500

    Added comments and docstrings to Timer class (lead: syed-safwaan)

--- utils/timer.py
+++ utils/timer.py
@@ -5,35 +5,45 @@ from utils import timeutils
 
 class Timer:
     def __init__(self, userid: str, time_delta_mins: int, msg: str, discord_message, include_seconds=False):
+        """ Initialize attributes of Timer instance. """
+
         self._userid = userid
-        self._start_time = dt.datetime.utcnow()
+        self._start_time = dt.datetime.utcnow()  # UTC time standard
         self._end_time = self.start_time + td(minutes=time_delta_mins)
         if not include_seconds:
             self._start_time = timeutils.str_to_datetime(self._start_time.strftime("%Y-%m-%d %H:%M"))
             self._end_time = timeutils.str_to_datetime(self._end_time.strftime("%Y-%m-%d %H:%M"))
-        self._msg = msg
-        self._discord_message = discord_message
+        self._msg = msg  # remaining message text
+        self._discord_message = discord_message  # message object
 
     def __repr__(self):
+        """ Get string representation of Timer instance information. """
+
         return f"Timer (User: {self.userid}) " \
                f"[ {self.end_time} | \"{self.msg}\" " \
                f"| Created at {self.start_time} " \
                f"| With {self.time_remaining()} time remaining ]"
 
+    # ----- PROPERTIES ----- #
+
     @property
     def userid(self):
+        """ ID of user requesting Timer creation. """
         return self._userid
 
     @property
     def start_time(self):
+        """ Time of Timer creation. """
         return self._start_time
 
     @property
     def end_time(self):
+        """ Timer activation datetime. """
         return self._end_time
 
     @property
     def msg(self):
+        """ Message text associated with Timer instance. """
         return self._msg
 
     @msg.setter
@@ -42,16 +52,26 @@ class Timer:
 
     @property
     def discord_message(self):
+        """ Message object from Timer creation request. """
         return self._discord_message
 
+    # ----- #
+
     def formatted_discord_message(self):
+        """ Returns message sent when Timer instance is activated. """
+
         return f"{self.discord_message.author.mention} Your timer for {str(self.end_time)} has finished. " \
                f"Here's your initial message: {self.msg}"
 
     def time_remaining(self):
+        """ Returns remaining time until Timer activation. """
+
         return self.end_time - dt.datetime.utcnow()
 
     def embed_repr(self):
+
+        """ Returns Discord Embed representation for message sent when Timer instance is activated. """
+
         pass
 
     def __lt__(self, obj):
