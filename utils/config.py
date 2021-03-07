b41f8990147e412c23e32e6b01c63ecf771696ac
Mirza Saim Beg <m.saim.beg@gmail.com>
2021-03-07 00:13:47 -0500

    improved implementation of dev check

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,10 @@
 from utils import timer_priority_queue
 from collections import defaultdict as dd
 
-
 timer_pqueue = timer_priority_queue.TimerPriorityQueue()
 note_dict = dd(list)
+author_ids = [189533543993442304,
+              154752622262353921,
+              667203504196026422,
+              359496559773351936,
+              487272627006734339]
