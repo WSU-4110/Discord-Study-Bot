97adce11ef051520f5dfe3edee5cf9f32ea3d544
Syed Safwaan <syed-safwaan@wayne.edu>
2021-03-08 23:04:59 -0500

    Working on Profile initialization in Profile cog (lead: syed-safwaan, contributor: adisai)

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,6 @@
 from utils import timer_priority_queue
 from collections import defaultdict as dd
+from typing import *
 
 timer_pqueue = timer_priority_queue.TimerPriorityQueue()
 note_dict = dd(list)
@@ -8,3 +9,10 @@ author_ids = [189533543993442304,
               667203504196026422,
               359496559773351936,
               487272627006734339]
+
+
+def hex_to_int(colhex: str) -> int:
+    inthex = int(colhex.replace('#', ''), 16)
+    intcol = int(hex(inthex), 0)
+
+    return intcol
