817669b9ba8b392f7c5dc33cd460af4868e6eb4d
Syed Safwaan <syed-safwaan@wayne.edu>
2021-03-10 21:09:16 -0500

    Added Colors named tuple (later embed use) (lead: syed-safwaan)

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,5 @@
 from utils import timer_priority_queue
-from collections import defaultdict as dd
+from collections import defaultdict as dd, namedtuple as nt
 from typing import *
 
 timer_pqueue = timer_priority_queue.TimerPriorityQueue()
@@ -16,3 +16,18 @@ def hex_to_int(colhex: str) -> int:
     intcol = int(hex(inthex), 0)
 
     return intcol
+
+
+class Colors(NamedTuple):
+    WSU_GREEN: int  # general info
+    WSU_GOLD: int  # user prompts
+    ERROR: int  # input errors
+    TIMEOUT: int  # input timeout
+
+
+colors = Colors(
+    WSU_GREEN=0x00584D,
+    WSU_GOLD=0XFFC842,
+    ERROR=0xFF0000,
+    TIMEOUT=0x777777
+)
