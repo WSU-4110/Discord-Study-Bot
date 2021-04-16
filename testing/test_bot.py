64286350c61dbe6b6682132bad29a74706dbb474
Mirza Saim Beg <m.saim.beg@gmail.com>
2021-04-15 23:37:41 -0400

    Unit test for Search cog and EasterEgg cog commands (Lead: msaimbeg)

--- testing/test_bot.py
+++ testing/test_bot.py
@@ -22,6 +22,10 @@ EMBED_MESSAGE_CLASS = 'grid-1nZz7S'
 EMBED_TITLE_CLASS = 'embedTitle-3OXDkz'
 EMBED_LINKED_TITLE_CLASS = 'anchor-3Z-8Bb'
 EMBED_MESSAGE_BODY_CLASS = 'embedDescription-1Cuq9a'
+EMBED_FIELD_NAME_CLASS = "embedFieldName-NFrena"
+
+REACTION_CLASS = "reactions-12N0jA"
+INDIVIDUAL_REACTION_CLASS = "reaction-1hd86g"
 
 
 @pytest.fixture(scope="session")
@@ -39,38 +43,6 @@ def driver():
     time.sleep(REQUEST_WAIT_TIME)
     yield driver  # allow for tests to run
     driver.quit()  # teardown by quitting the webdriver instance
-
-
-'''
-def test_unit_test_channel(driver: webdriver.Chrome):
-    text = driver.find_element_by_xpath('/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div[2]/main/div[1]/div/div/div/div[1]/h1').text
-    assert text == 'Welcome to #unit-testing!'
-'''
-
-
-# def test_ping_command(driver: webdriver.Chrome):
-#     driver.find_element_by_xpath(TEXT_INPUT_XPATH).send_keys('sb!ping' + Keys.RETURN)
-#     time.sleep(REQUEST_WAIT_TIME)
-#     messages = driver.find_element_by_xpath(MESSAGE_CONTAINER_XPATH).find_elements_by_class_name(TEXT_MESSAGE_CLASS)
-#     message_text = messages[-1].find_element_by_class_name(TEXT_MESSAGE_BODY_CLASS).text
-#     assert message_text == 'pong!'
-
-
-# def test_create_todo(driver: webdriver.Chrome):
-#     driver.find_element_by_xpath(TEXT_INPUT_XPATH).send_keys('sb!create-item 1 test' + Keys.RETURN)
-#     time.sleep(REQUEST_WAIT_TIME)
-#     messages = driver.find_element_by_xpath(MESSAGE_CONTAINER_XPATH).find_elements_by_class_name(TEXT_MESSAGE_CLASS)
-#     message_text = messages[-1].find_element_by_class_name(EMBED_MESSAGE_CLASS).find_element_by_class_name(
-#         EMBED_MESSAGE_BODY_CLASS).text
-#     assert message_text == 'ToDoList Item created!'
-#     driver.get(DM_CHANNEL_URL)
-#     time.sleep(REQUEST_WAIT_TIME)
-#     time.sleep(COMMAND_WAIT_TIME)
-#     messages = driver.find_elements_by_class_name(TEXT_MESSAGE_CLASS)
-#     message_text = messages[-1].find_element_by_class_name(TEXT_MESSAGE_BODY_CLASS).text
-#     assert 'To Do Item' in message_text
-#     driver.get(UNIT_TEST_CHANNEL_URL)
-#     time.sleep(REQUEST_WAIT_TIME)
 
 
 def test_default_search(driver: webdriver.Chrome):
@@ -133,7 +105,7 @@ def test_search_site(driver: webdriver.Chrome):
     assert 'banana' in message_text5.lower()
 
 
-def test_default_search(driver: webdriver.Chrome):
+def test_big_search(driver: webdriver.Chrome):
     driver.find_element_by_xpath(TEXT_INPUT_XPATH).send_keys('sb!big-search banana' + Keys.RETURN)
     time.sleep(REQUEST_WAIT_TIME)
     messages = driver.find_element_by_xpath(MESSAGE_CONTAINER_XPATH).find_elements_by_class_name(TEXT_MESSAGE_CLASS)
@@ -141,3 +113,88 @@ def test_default_search(driver: webdriver.Chrome):
         EMBED_TITLE_CLASS).find_element_by_class_name(EMBED_LINKED_TITLE_CLASS).get_attribute('href')
     assert 'https://letmegooglethat.com/?q=banana' in message_text.lower()
 
+
+def test_eight_ball(driver: webdriver.Chrome):
+    responses = [
+        'It is certain.',
+        'It is decidedly so.',
+        'Without a doubt',
+        'Yes - definetly',
+        'You may rely on it.',
+        'As I see it, yes.',
+        'Most likely.',
+        'Outlook good.',
+        'Yes.',
+        'Signs point to yes.',
+        'Reply hazy, try again',
+        'Ask again later.',
+        'Better not tell you now.',
+        'Cannot predict now',
+        'Concentrate and ask again.',
+        'My reply is no.',
+        'My sources say no.',
+        'Outlook not so good.',
+        'Very doubtful.'
+    ]
+    driver.find_element_by_xpath(TEXT_INPUT_XPATH).send_keys('sb!8-ball did this work' + Keys.RETURN)
+    time.sleep(REQUEST_WAIT_TIME)
+    messages = driver.find_element_by_xpath(MESSAGE_CONTAINER_XPATH).find_elements_by_class_name(TEXT_MESSAGE_CLASS)
+    message_text = messages[-1].find_element_by_class_name(EMBED_MESSAGE_CLASS).find_element_by_class_name(
+        EMBED_FIELD_NAME_CLASS).text
+    assert message_text in responses
+
+
+def test_rock_paper_scissors(driver: webdriver.Chrome):
+    valid_responses = [
+        "Yay! you won.",
+        "Oh no! you lost.",
+        "Tied!"
+    ]
+
+    driver.find_element_by_xpath(TEXT_INPUT_XPATH).send_keys('sb!rock-paper-scissors' + Keys.RETURN)
+    time.sleep(REQUEST_WAIT_TIME)
+    messages = driver.find_element_by_xpath(MESSAGE_CONTAINER_XPATH).find_elements_by_class_name(TEXT_MESSAGE_CLASS)
+    message_text = messages[-1].find_element_by_class_name(EMBED_MESSAGE_CLASS).find_element_by_class_name(
+        EMBED_TITLE_CLASS).text
+    assert message_text == "Choose rock paper or scissors!"
+
+    time.sleep(REQUEST_WAIT_TIME/2)
+    messages[-1].find_element_by_class_name(REACTION_CLASS).find_element_by_class_name(
+        INDIVIDUAL_REACTION_CLASS).click()
+    time.sleep(REQUEST_WAIT_TIME/2)
+
+    messages = driver.find_element_by_xpath(MESSAGE_CONTAINER_XPATH).find_elements_by_class_name(TEXT_MESSAGE_CLASS)
+    message_text = messages[-1].find_element_by_class_name(EMBED_MESSAGE_CLASS).find_element_by_class_name(
+        EMBED_TITLE_CLASS).text
+    assert message_text in valid_responses
+
+
+def test_guessing_game(driver: webdriver.Chrome):
+    valid_responses = [
+        "You gave up!",
+        "You guessed wrong! Try again.",
+        "Guess the number!",
+    ]
+
+    driver.find_element_by_xpath(TEXT_INPUT_XPATH).send_keys('sb!guessing-game' + Keys.RETURN)
+    time.sleep(REQUEST_WAIT_TIME)
+
+    flag = True
+    while flag:
+        messages = driver.find_element_by_xpath(MESSAGE_CONTAINER_XPATH).find_elements_by_class_name(TEXT_MESSAGE_CLASS)
+        message_text = messages[-1].find_element_by_class_name(EMBED_MESSAGE_CLASS).find_element_by_class_name(
+            EMBED_TITLE_CLASS).text
+
+        if message_text == "Yay! you won.":
+            assert message_text == "Yay! you won."
+            flag = False
+        elif message_text == "Oh no! you lost.":
+            assert message_text == "Oh no! you lost."
+            flag = False
+        else:
+            assert message_text in valid_responses
+
+            time.sleep(REQUEST_WAIT_TIME / 2)
+            messages[-1].find_element_by_class_name(REACTION_CLASS).find_element_by_class_name(
+                INDIVIDUAL_REACTION_CLASS).click()
+            time.sleep(REQUEST_WAIT_TIME)
