678af857f219e78af3ed7cbe32516e59776b1813
Bryan Vinson <hc7391@wayne.edu>
2021-04-20 23:17:40 -0400

    FR8: Input Sanitizaton to check for user roles. Evaluates whether to send personal or group message.

--- models/inspire.py
+++ models/inspire.py
@@ -7,10 +7,11 @@ import random
 import json
 from models import base_db_model
 
-
+#This class uses both json and web scraping to get quotes from the internet and send to users
 class Inspire(base_db_model.BaseDBModel):
     def __init__(self):
         pass
+
     def get_cs_satire(self):
         url = 'http://quotes.stormconsultancy.co.uk/random.json'
         r = requests.get(url)
@@ -23,22 +24,41 @@ class Inspire(base_db_model.BaseDBModel):
         quote = r.json()
         return quote[0]['a'] + ': ' + quote[0]['q']
 
+    # This method uses web scraping techniques to get quotes
     def get_cs_motivated(self):
-        rand_num = random.randint(1, 189)
         authors = []
         quotes = []
         url = 'https://fortrabbit.github.io/quotes/'
         webpage = requests.get(url)
         soup = BeautifulSoup(webpage.text, "html.parser")
-        quote_text = soup.findAll('p', attrs={'class': 'type-l p-m'})
-        quote_author = soup.findAll('div', attrs={'class': 'm-top-m'})
+        #try catch around web scraping in case html tags have been modified
+        try:
+            quote_text = soup.findAll('p', attrs={'class': 'type-l p-m'})
+            quote_author = soup.findAll('div', attrs={'class': 'm-top-m'})
+        except Exception as e:
+            print(e)
 
-        for q in quote_author:
-            author = q.text.strip().split('\n')[0]
+        # For loops strip content of any white space and brackets
+        for author_name in quote_author:
+            author = author_name.text.strip().split('\n')[0]
             authors.append(author)
 
-        for i in quote_text:
-            quote = i.text.strip().split('\n')[0]
+        for quote_content in quote_text:
+            quote = quote_content.text.strip().split('\n')[0]
             quotes.append(quote)
 
+        #Try catch in case number of quotes changes
+        try:
+            rand_num = random.randint(1, len(quotes))
+        except Exception as e:
+            print(e)
+
         return authors[rand_num] + ': ' + quotes[rand_num]
+
+    # parses the user input from command and returns a string containing the user roles
+    def parse_role(self, discord_role: str):
+        role = ''
+        for ele in discord_role.split(' '):
+            if '<@&' in ele and '>' in ele:
+                role += ele + ' '
+        return role
