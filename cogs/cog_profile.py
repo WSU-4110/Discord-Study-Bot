4e13a8296ce34451da6e9ef04cde7e37465c5153
Syed Safwaan <syed-safwaan@wayne.edu>
2021-04-22 01:22:35 -0400

    Implemented user profile (timezone saving) (lead: syed-safwaan)

--- cogs/cog_profile.py
+++ cogs/cog_profile.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 import os
 from dotenv import load_dotenv
-from utils import time_utils, config as cfg
+from utils import time_utils, database_utils as dbutils, config as cfg
 from factories.embedfactory import EmbedFactory
 
 
@@ -32,23 +32,7 @@ class ProfileCommands(commands.Cog, name='Profile Commands'):
             icon_url=ctx.author.avatar_url
         ).set_footer(text=self.bot.user.display_name, icon_url=self.bot.user.avatar_url))
 
-        # await ctx.send(embed=discord.Embed(
-        #     description="Please enter your time zone information as a 3-letter code (eg. EST, PST).\n",
-        #     colour=cfg.colors.WSU_GOLD
-        # ))
-
         while True:
-            # try:
-            #     response = await self.bot.wait_for('message', check=check, timeout=60)
-            #     if await self.change_timezone(ctx, response.content):
-            #         break
-            # except asyncio.TimeoutError as e:
-            #     await ctx.send(embed=discord.Embed(
-            #         description="Took too long to respond!",
-            #         colour=cfg.colors.TIMEOUT
-            #     ))
-            #     break
-
             tz = await EmbedFactory.prompt(ctx, self.bot,
                                            "Please enter your time zone information as a 3-letter code (eg. EST, PST)",
                                            check=check)
@@ -64,14 +48,23 @@ class ProfileCommands(commands.Cog, name='Profile Commands'):
         Changes the user's saved timezone.
         """
 
+        user = ctx.author
+
         if tz in time_utils.tz_map:
-            await EmbedFactory.success(ctx, "nice")
+            con = dbutils.connection()
+            cursor = con.cursor(buffered=True)
+            if user.id in cfg.user_tzs:
+                cursor.execute(
+                    "UPDATE USER_INFO SET tz = %s WHERE USER_ID = %s",
+                    (tz, user.id))
+            else:
+                cursor.execute("INSERT INTO USER_INFO (user_id, tz) values (%s, %s)", (user.id, tz))
+            con.commit()
+            cursor.close()
+            cfg.user_tzs[user.id] = tz
+            await EmbedFactory.success(ctx, "Time zone information saved!")
             return True
         else:
-            # await ctx.send(embed=discord.Embed(
-            #     description="Time zone not recognized!",
-            #     colour=cfg.colors.ERROR
-            # ))
             await EmbedFactory.error(ctx, "Time zone not recognized!")
             return False
 
@@ -86,6 +79,18 @@ class ProfileCommands(commands.Cog, name='Profile Commands'):
         """
         Displays the user's general information.
         """
+        user = ctx.author
+
+        await ctx.send(embed=discord.Embed(
+            title="User Info",
+            colour=cfg.colors.WSU_GREEN
+        ).set_author(
+            name=ctx.author.display_name,
+            icon_url=ctx.author.avatar_url
+        ).add_field(
+            name="Time Zone",
+            value=cfg.user_tzs[user.id]
+        ))
 
     @commands.command(name="send-feedback")
     async def send_feedback(self, ctx, *, msg):
