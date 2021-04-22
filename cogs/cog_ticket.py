34b7daa8947eacec550bc3794615dc98c620fcc2
Syed Safwaan <syed-safwaan@wayne.edu>
2021-04-21 23:14:56 -0400

    Removed redundant code and comments (lead: syed-safwaan)

--- cogs/cog_ticket.py
+++ cogs/cog_ticket.py
@@ -25,28 +25,10 @@ class TicketCommands(commands.Cog, name='Ticket Commands'):
 
         # If this server already has a ticket category, exit
         if cfg.server_ticket_ctgs.get(guild.id) is not None:
-            # await ctx.send(embed=discord.Embed(
-            #     description="This server already has tickets set up!",
-            #     colour=cfg.colors.ERROR
-            # ))
             await EmbedFactory.error(ctx, "This server already has tickets set up!")
             return
 
         # Prompt the user for the name of the ticket category to be made, cancel if it times out
-        # await ctx.send(embed=discord.Embed(
-        #     description="Enter the name for the ticket category for this server.",
-        #     colour=cfg.colors.WSU_GOLD
-        # ))
-        #
-        # try:
-        #     response = await self.bot.wait_for('message', check=lambda rctx: rctx.author == ctx.author, timeout=60)
-        # except asyncio.TimeoutError as e:
-        #     await ctx.send(embed=discord.Embed(
-        #         description="Took too long to respond!",
-        #         colour=cfg.colors.TIMEOUT
-        #     ))
-        #     return
-
         name = await EmbedFactory.prompt(ctx, self.bot, "Enter the name for the ticket category for this server.")
         if name is None:
             return
@@ -68,11 +50,6 @@ class TicketCommands(commands.Cog, name='Ticket Commands'):
         con.commit()
         cursor.close()
 
-        # await ctx.send(embed=discord.Embed(
-        #     description="Ticket category created!",
-        #     colour=cfg.colors.SUCCESS
-        # ))
-
         await EmbedFactory.success(ctx, "Ticket category created!")
 
     @commands.command(name="create-ticket", aliases=["ctkt"])
@@ -89,10 +66,6 @@ class TicketCommands(commands.Cog, name='Ticket Commands'):
         # Get ticket category (if not exists, exit)
         category_id = cfg.server_ticket_ctgs.get(guild.id)
         if category_id is None:
-            # await ctx.send(embed=discord.Embed(
-            #     description="This server doesn't have a ticket category set up yet! Use `setup-tickets` to make one.",
-            #     colour=cfg.colors.ERROR
-            # ))
             await EmbedFactory.error(ctx,
                                      "This server doesn't have a ticket category set up yet! Use `setup-tickets` to make one.")
             return
@@ -106,46 +79,13 @@ class TicketCommands(commands.Cog, name='Ticket Commands'):
         await ticket_channel.set_permissions(ctx.author, read_messages=True, send_messages=False, embed_links=True,
                                              attach_files=True)
 
-        # q_prompt = await ticket_channel.send(ctx.author.mention, embed=discord.Embed(
-        #     description="Type in your question here.",
-        #     colour=cfg.colors.WSU_GOLD
-        # ))
-        #
-        # try:
-        #     q_response = await self.bot.wait_for('message', check=lambda
-        #         rctx: rctx.author == ctx.author and rctx.channel.id == ticket_channel.id, timeout=300)
-        # except asyncio.TimeoutError as e:
-        #     await ctx.send(embed=discord.Embed(
-        #         description="Took too long to respond!",
-        #         colour=cfg.colors.TIMEOUT
-        #     ))
-        #     ticket_channel.delete()
-        #     return
-
         # Continue setup in the ticket channel by prompting user for their question, delete everything if timed out
         question = await EmbedFactory.prompt(ticket_channel, self.bot, "Type in your question here.", check=lambda
             rctx: rctx.author == ctx.author and rctx.channel.id == ticket_channel.id,
                                              body=ctx.author.mention, timeout=300, delete=True)
-        print(question)
         if question is None:
             await ticket_channel.delete()
             return
-
-        # r_prompt = await ticket_channel.send(embed=discord.Embed(
-        #     description="Which roles would you like to ping with this question? Separate each ping with spaces.",
-        #     colour=cfg.colors.WSU_GOLD
-        # ))
-        #
-        # try:
-        #     r_response = await self.bot.wait_for('message', check=lambda
-        #         rctx: rctx.author == ctx.author and rctx.channel.id == ticket_channel.id, timeout=30)
-        # except asyncio.TimeoutError as e:
-        #     await ctx.send(embed=discord.Embed(
-        #         description="Took too long to respond!",
-        #         colour=cfg.colors.TIMEOUT
-        #     ))
-        #     ticket_channel.delete()
-        #     return
 
         # Prompt users for the roles they want to ping in the question statement, delete everything if timed out
         roles = await EmbedFactory.prompt(ticket_channel, self.bot,
@@ -158,10 +98,6 @@ class TicketCommands(commands.Cog, name='Ticket Commands'):
             return
 
         roles = roles.split()
-
-        # # Extract text from question and roles responses
-        # question = q_response.content
-        # roles = r_response.content.split()
 
         # For each specific role, give it read/send/link/file permissions
         for role_string in roles:
@@ -203,20 +139,11 @@ class TicketCommands(commands.Cog, name='Ticket Commands'):
 
         # If the channel is not a ticket channel (no ticket associated with it), exit
         if resolved_ticket is None:
-            # await ctx.send(embed=discord.Embed(
-            #     description="Not a ticket channel!",
-            #     colour=cfg.colors.ERROR
-            # ))
             await EmbedFactory.error(ctx, "Not a ticket channel!")
             return
 
         # Notify creator of ticket the channel is deleted
         creator = discord.utils.get(guild.members, id=resolved_ticket.user_id)
-        # await creator.send(embed=discord.Embed(
-        #     description=f"Your ticket was resolved{'.' if not msg else f' with message: {msg}'}",
-        #     colour=cfg.colors.WSU_GREEN
-        # ))
-
         await EmbedFactory.success(creator, f"Your ticket was resolved{'.' if not msg else f' with message: {msg}'}")
 
         resolved_ticket.delete(resolved_ticket.message_id)
